d13de7d2374af6b649f641cd66cce5749b8ffbdf
MdSaayed <sayedkhan1786@gmail.com>
2024-02-12 15:22:13 +0600

    Testing command

--- models/helpers/tokens.js
+++ models/helpers/tokens.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-// generate we token
+// generate we token update
 exports.generateToken=(payload,expired)=>{
     return jwt.sign(payload,process.env.TOKEN_SECRET,{
         expiresIn:expired
