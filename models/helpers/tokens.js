d90a653f7ffb615d268deb4aa7545beac9ccf1bb
MdSaayed <sayedkhan1786@gmail.com>
2024-02-12 15:18:02 +0600

    Update generate token code

--- models/helpers/tokens.js
+++ models/helpers/tokens.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+// generate we token
 exports.generateToken=(payload,expired)=>{
     return jwt.sign(payload,process.env.TOKEN_SECRET,{
         expiresIn:expired
